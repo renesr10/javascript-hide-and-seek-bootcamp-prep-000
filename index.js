bac55e62afd372137cbbed9ad0620cbeec1152a7
Rene Santana <renesantana10@gmail.com>
2020-05-05 01:10:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var no = document.querySelectorAll("#grand-node div");
+  return no[no.length - 1];
+  
+} 
